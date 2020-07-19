b24ba36c73943f9ee82706c9bb0dbf8319270888
Liao <w09032222@gmail.com>
2020-07-19 23:45:45 +0900

    Add files via upload

--- form.php
+++ form.php
@@ -8,13 +8,28 @@
 </head>
 
 <body>
+    <!-- use form to send data -->
+    <!-- action="target PHP" -->
+    <!-- !! writing without security is dangerous !! -->
+    <!-- method="the HTTP method for collecting data, usually POST" -->
+    <!-- the name="" inside input tag will become a key for $_POST global array -->
+    <!-- and the value which user entered will be saved as its value -->
     <form action="form.php" method="POST">
-        <input type="text" placeholder="enter here" />
+        <p>text must be longer than 5 characters.</p>
+        <input type="text" name="text" placeholder="enter here" />
+        <br>
         <input type="submit" name="submit" />
     </form>
+    <!-- response to the request -->
     <?php
     if (isset($_POST['submit'])) {
-        echo "text entered";
+        $text = $_POST['text'];
+        $minlen = 5;
+        if (strlen($text) <= $minlen) {
+            echo "please enter more than 5 characters.";
+        } else {
+            echo $text . " was entered.";
+        }
     }
     ?>
 </body>
